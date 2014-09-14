38d9e771222a6bb2d0e41d77965f0a591b3a120d
ZhangBohan <imzhangbohan@gmail.com>
2014-09-14 23:37:41 +0800

    add log support

--- src/main/java/com/jiajiaohello/HelloController.java
+++ src/main/java/com/jiajiaohello/HelloController.java
@@ -1,15 +1,19 @@
 package com.jiajiaohello;
 
+import static com.jiajiaohello.support.utils.Utils.LOG;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+        LOG.info("welcome init");
 		model.addAttribute("message", "你好，世界!");
 		return "hello";
 	}
---
+++ src/main/java/com/jiajiaohello/support/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.jiajiaohello.support.utils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * User: bohan
+ * Date: 9/14/14
+ * Time: 11:36 PM
+ */
+public class Utils {
+    public static final Log LOG = LogFactory.getLog("root");
+}
