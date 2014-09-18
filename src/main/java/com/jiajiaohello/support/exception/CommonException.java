a49fe3386eb9131c535435ca351390cd7bdf2e3c
ZhangBohan <imzhangbohan@gmail.com>
2014-09-18 21:46:14 +0800

    update exception

--- src/main/java/com/jiajiaohello/support/exception/CommonException.java
+++ src/main/java/com/jiajiaohello/support/exception/CommonException.java
@@ -12,16 +12,4 @@ public class CommonException extends Exception {
     public CommonException(String message) {
         super(message);
     }
-
-    public CommonException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CommonException(Throwable cause) {
-        super(cause);
-    }
-
-    public CommonException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
