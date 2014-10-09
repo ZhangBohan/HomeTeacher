899fedb561b0fcc36b0583b8db2df9d479d701dc
ZhangBohan <imzhangbohan@gmail.com>
2014-10-09 21:40:52 +0800

    license

--- src/main/java/com/jiajiaohello/support/exception/TeacherInfoNotFillException.java
+++ src/main/java/com/jiajiaohello/support/exception/TeacherInfoNotFillException.java
@@ -6,4 +6,5 @@ package com.jiajiaohello.support.exception;
  * Time: 11:31 PM
  */
 public class TeacherInfoNotFillException extends UserLogicException {
+    private static final long serialVersionUID = -6947971712767999037L;
 }
